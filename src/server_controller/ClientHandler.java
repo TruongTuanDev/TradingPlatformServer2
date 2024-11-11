c6697cdd0edaf3a09197653c47ea1d7d7d2fc7b9
TruongTuanDev <tuanhulonhat@gmail.com>
2024-11-11 16:17:10 +0700

    load coinnn

--- src/server_controller/ClientHandler.java
+++ src/server_controller/ClientHandler.java
@@ -33,68 +33,88 @@ public class ClientHandler extends Thread {
         this.connectTime = connectTime;
         this.server = server;
     }
-
     @Override
     public void run() {
         userService = new UserService();
         tokenService = new TokenService();
         try {
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            ObjectOutputStream objectWriter = new ObjectOutputStream(clientSocket.getOutputStream()); 
+            ObjectOutputStream objectWriter = new ObjectOutputStream(clientSocket.getOutputStream());
+
             String message;
             while ((message = in.readLine()) != null) {
                 System.out.println("Client gửi: " + message);
-                String email="";
+                String email = "";
                 String[] messageSplit = message.split(",");
                 int lengthMessage = messageSplit.length;
-	            String username = messageSplit[1];
-	            String password = messageSplit[2];
-	            if(lengthMessage==4) {
-	            	email = messageSplit[3];
-	            }
-             
+                
+                String username = null;
+                String password = null;
+                if (lengthMessage > 1) {
+                	username= messageSplit[1];
+                	password= messageSplit[2];
+                }
+               
+                if (lengthMessage == 4) {
+                    email = messageSplit[3];
+                }
+
                 Object[] row = {clientIP, username, email, connectTime};
-//                Object[] row = {1, 1, 1, 1};
-	            tableModel.addRow(row);
+                tableModel.addRow(row);
 
-                // Xử lý thông điệp và thực hiện hành động
                 switch (messageSplit[0]) {
-                    case "request-login":
-                        boolean check=userService.checkUser(username, password);
-                        System.out.println(check);
-                        if(check) {
-                        	out.println("login-success"+","+username);
-                        }else {
-                        	out.println("login-fail");
-                        }
-                        break;
-
-                    case "request-register":
-                        User user = userService.createUser(username, password, email);
-                        if (user != null) {
-                            out.println("register-success");
-                        } else {
-                            out.println("register-false");
-                        }
-                        break;
-                    case "request-getlistcoin" :
-                    	List<Token> tokens = tokenService.getListTokens();
-                    	if(tokens != null) {
-                    		objectWriter.writeObject(tokens);
-                    		objectWriter.flush();
-                    		out.println("getlistcoin-success");
+                case "request-login":
+                    // Kiểm tra đăng nhập và gửi phản hồi login
+                    boolean check = userService.checkUser(username, password);
+                    String responseLogin = check ? "login-success," + username : "login-fail";
+                    
+                    // Gửi phản hồi đăng nhập qua ObjectOutputStream
+                    objectWriter.writeObject(responseLogin);
+                    objectWriter.flush();
+                    
+                    // Nếu đăng nhập thành công, gửi danh sách token
+                    if (check) {
+                        List<Token> tokens = tokenService.getListTokens();
+                        if (tokens != null) {
+                            // In danh sách token để kiểm tra
+                            for (Token token : tokens) {
+                                System.out.println(token.toString());
+                            }
+                            // Gửi danh sách token qua ObjectOutputStream
+                            objectWriter.writeObject(tokens);
+                            objectWriter.flush();
                         } else {
                             out.println("getlistcoin-false");
                         }
-                    	break;
-                    	
-                    case "request-check-username":
-                        // Thực hiện kiểm tra tên người dùng (nếu cần)
+                    }
+                    
+                    break;
+
+                    case "request-register":
+                        User user = userService.createUser(username, password, email);
+                        String responseRegister = (user != null) ? "register-success" : "register-false";
+                        objectWriter.writeObject(responseRegister); // Gửi thông báo qua ObjectOutputStream
+                        objectWriter.flush();
                         break;
 
+                    case "request-getlistcoin" :
+//                        List<Token> tokens = tokenService.getListTokens();
+//                        for (Token token : tokens) {
+//							System.out.println(token.toString());
+//						}
+//                        if (tokens != null) {
+//                            objectWriter.writeObject(tokens);
+//                            objectWriter.flush();  // Đảm bảo đẩy dữ liệu qua luồng
+////                            out.println("getlistcoin-success");
+//                            objectWriter.flush();
+//                        } else {
+//                            out.println("getlistcoin-false");
+//                        }
+//                       
+//                        break;
                     default:
-                        throw new IllegalArgumentException("Unexpected value: " + messageSplit[0]);
+                        objectWriter.writeObject("Unexpected command: " + messageSplit[0]);
+                        objectWriter.flush();
                 }
             }
         } catch (IOException e) {
@@ -105,8 +125,8 @@ public class ClientHandler extends Thread {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            // Xóa client khỏi danh sách và cập nhật số lượng client
             server.removeClient(clientSocket);
         }
     }
+
 }
